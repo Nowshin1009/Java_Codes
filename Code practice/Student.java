41c8d469beda9de895e43e1a1ea13f199a3c30c7
Nowshin <105603725+Nowshin1009@users.noreply.github.com>
2024-01-22 23:48:10 +0600

    Update Student.java
    Question: Assign and print the roll number, phone number and address of two students having names "Sam" and "John" respectively by creating two objects of class 'Student'.

--- Code practice/Student.java
+++ Code practice/Student.java
@@ -1,3 +1,6 @@
+/*Assign and print the roll number, phone number and address of two students having names "Sam" and "John"
+    respectively by creating two objects of class 'Student'.*/
+
 public class Student {
     String name;
     int roll_no;
